4c706f5ee97efce1b0b5978079fa48c0f6913366
unknown <Ning Ding>
2022-05-25 00:24:46 +0800

    初始化项目结构

---
+++ src/main/java/team/snof/simplesearch/SimpleSearchApplication.java
@@ -0,0 +1,13 @@
+package team.snof.simplesearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team/snof/simplesearch/SimpleSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package team.snof.simplesearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
