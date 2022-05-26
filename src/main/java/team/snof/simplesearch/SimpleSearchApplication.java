1f3bf5ca805bc4421292b91200bdf36409f69ea0
EGO <7456081+Chewing_Gum@user.noreply.gitee.com>
2022-05-26 22:56:47 +0800

    //

--- src/main/java/team/snof/simplesearch/SimpleSearchApplication.java
+++ src/main/java/team/snof/simplesearch/SimpleSearchApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SimpleSearchApplication {
 
     public static void main(String[] args) {
+        //
         SpringApplication.run(SimpleSearchApplication.class, args);
     }
 
