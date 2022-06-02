2b221acfef9cf1e71ddbd9465f39dbc39d89a87d
JinChuan <447154317@qq.com>
2022-06-02 11:04:15 +0800

    feat:增加业务层排序逻辑

---
+++ src/main/java/team/snof/simplesearch/search/service/SearchService.java
@@ -0,0 +1,84 @@
+package team.snof.simplesearch.search.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+import team.snof.simplesearch.common.util.WordSegmentation;
+import team.snof.simplesearch.search.engine.Engine;
+import team.snof.simplesearch.search.model.vo.SearchListResponseVO;
+import team.snof.simplesearch.search.model.vo.SearchRequestVO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class SearchService {
+
+    @Autowired
+    WordSegmentation wordSegmentation;
+
+    // spring容器没启动的时候会爆红，没办法。直接用就行
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Autowired
+    SearchService searchService;
+
+    @Autowired
+    Engine engine;
+
+    public SearchListResponseVO search(SearchRequestVO request) {
+
+        // 查query结果
+        // Redis接口以参考这个 https://blog.csdn.net/AlbenXie/article/details/109348114
+        // 自己考虑具体缓存什么内容
+        redisTemplate.opsForHash().get();
+        if () {
+            // 返回结果
+        }
+
+        // 分词
+        List<String> words = wordSegmentation.segment(request.getQuery());
+
+        // 简单过滤
+        words.removeAll(request.getFilterWordList());
+
+        // 查缓存是否有分词的倒排索引
+        for (String word: words) {
+            // redisTemplate
+        }
+
+        // 调搜索引擎
+        if () { // 缓存里分词的索引不够
+            engine.getIndex();
+        }
+
+        // 缓存分词索引结果
+
+        // 调排序
+        // 王昱豪的排序逻辑，现在好像还没有，你和他对接一下
+        // List<Index> orderedIndex= order(List<List<Index>>)
+
+        // 根据索引的docid查出doc，也就是query的结果
+        // engine.batchGetDoc()
+
+        // 缓存query结果
+
+        return new SearchListResponseVO("结果对象");
+    }
+
+    public String test() {
+
+        String key = "key1";
+        List<String> value = new ArrayList<>();
+        value.add("1");
+        value.add("2");
+        value.add("3");
+        redisTemplate.opsForList().rightPushAll(key, value);
+
+        Long size = redisTemplate.opsForList().size(key);
+
+        return "Result: " + size;
+    }
+
+}
---
+++ src/main/java/team/snof/simplesearch/search/service/SortService.java
@@ -0,0 +1,74 @@
+package team.snof.simplesearch.search.service;
+
+import team.snof.simplesearch.search.model.dao.DocInfo;
+import team.snof.simplesearch.search.model.dao.Index;
+
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class SortService {
+    private static final double k_3 = 1.5;  // k3  1.2~2
+    public static void main(String []args){
+        test();
+    }
+    public static List<Long> order(List<String> query, List<Index> indexs) {
+        //1.计算分词在query出的出现次数
+        HashMap<String, Long> tf = new HashMap<>();
+        for (String word : query) {
+            tf.put(word, tf.getOrDefault(word, 0L) + 1);
+        }
+
+        //2.计算文档对应的相似度
+        HashMap<Long, BigDecimal> docSimilarity = new HashMap<>();
+        for (Index index : indexs) {
+            String word = index.getIndexKey();
+            for (DocInfo doc : index.getDocInfoList()) {
+                BigDecimal corr = doc.getCorr().multiply(new BigDecimal((k_3 + 1) * tf.get(word)))
+                        .divide(new BigDecimal(k_3 + tf.get(word)));
+                docSimilarity.put(doc.getDocId(), docSimilarity.getOrDefault(doc.getDocId(), new BigDecimal(0)).add(corr));
+            }
+
+        }
+
+        //3.按相似度从高到低排序
+        TreeMap<BigDecimal, Long> docs = new TreeMap<>();
+        for (Map.Entry<Long, BigDecimal> entry : docSimilarity.entrySet()) {
+            docs.put(entry.getValue(), entry.getKey());
+        }
+        docs.descendingMap();
+
+        List<Long> resultDocs = new ArrayList<>();
+        Iterator it = docs.keySet().iterator();
+        while (it.hasNext()) {
+            BigDecimal key = (BigDecimal) it.next();
+            resultDocs.add(docs.get(key));
+        }
+        return resultDocs;
+    }
+
+    private static void test(){
+        List<String> query = new ArrayList<>(2);
+        List<Index> indexs = new ArrayList<>(2);
+        query.add("test");query.add("namo");
+
+        List<DocInfo>  index1 = new ArrayList<>();
+        List<DocInfo>  index2 = new ArrayList<>();
+        index1.add(new DocInfo(1,3,new BigDecimal(0.5)));
+        index1.add(new DocInfo(2,4,new BigDecimal(0.6)));
+        index2.add(new DocInfo(1,3,new BigDecimal(0.4)));
+        index2.add(new DocInfo(2,4,new BigDecimal(0.3)));
+
+        indexs.add(new Index("test",index1));  indexs.add(new Index("namo",index2));
+
+        List<Long> docs = order(query,indexs);
+        for(Long docID:docs){
+            System.out.printf("%d\n",docID);
+        }
+    }
+}
