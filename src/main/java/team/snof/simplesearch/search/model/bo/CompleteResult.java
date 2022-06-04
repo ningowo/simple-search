8671477cfdbbe4e819c9dde44f1e2b1a44b0e62a
丁宁 <dingning057@163.com>
2022-06-04 18:42:18 +0800

    feat: 优化SearchService，更新engine接口返回值

--- src/main/java/team/snof/simplesearch/search/model/bo/CompleteResult.java
+++ src/main/java/team/snof/simplesearch/search/model/bo/CompleteResult.java
@@ -1,9 +1,11 @@
 package team.snof.simplesearch.search.model.bo;
 
+import lombok.Data;
 import team.snof.simplesearch.search.model.dao.doc.Doc;
 
 import java.util.List;
 
+@Data
 public class CompleteResult {
     List<Doc> totalDocs;
     List<Long> totalDocIds;
--- src/main/java/team/snof/simplesearch/search/model/bo/CompleteResultWithRange.java
+++ src/main/java/team/snof/simplesearch/search/model/bo/CompleteResultWithRange.java
@@ -1,11 +1,17 @@
 package team.snof.simplesearch.search.model.bo;
 
+import lombok.Data;
 import team.snof.simplesearch.search.model.dao.doc.Doc;
 
 import java.util.List;
 
+@Data
 public class CompleteResultWithRange {
+
     List<Doc> docs;
+
     List<Long> totalDocIds;
+
     List<String> relatedSearch;
+
 }
