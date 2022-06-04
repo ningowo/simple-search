d809480f41e57871b7dfd8448de0ee80d14fd6b9
chenluWang <wangchenlu@hyperchain.cn>
2022-06-04 21:15:53 +0800

    style: Integrate folder

--- src/main/java/team/snof/simplesearch/SimpleSearchApplication.java
+++ src/main/java/team/snof/simplesearch/SimpleSearchApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import team.snof.simplesearch.repository.RoleRepository;
+import team.snof.simplesearch.user.repository.RoleRepository;
 import team.snof.simplesearch.search.model.bo.favorite.Role;
 import team.snof.simplesearch.search.model.bo.favorite.RoleName;
 
