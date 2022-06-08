ebc7a1c096813125781ac57e57b4f067143cf5db
ningd <55850050+ningowo@users.noreply.github.com>
2022-06-07 20:37:20 -0700

    Revert "Search engine稳定版v1"

--- src/main/java/team/snof/simplesearch/SimpleSearchApplication.java
+++ src/main/java/team/snof/simplesearch/SimpleSearchApplication.java
@@ -2,11 +2,7 @@ package team.snof.simplesearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableRedisRepositories
-@EnableSwagger2
 @SpringBootApplication
 public class SimpleSearchApplication {
 
