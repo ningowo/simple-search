5f57dd37b011d947c396390bcc761bb33c6a268e
ningd <55850050+ningowo@users.noreply.github.com>
2022-06-08 06:05:29 -0700

    Tiger search engine 修复mongotemplate的bug (#36)
    * fix：相关搜索的实现comment
    
    * fix：修改索引存储的bug
    
    Co-authored-by: Tigershan <2996529874@qq.com>

--- src/main/java/team/snof/simplesearch/search/storage/IndexPartialStorage.java
+++ src/main/java/team/snof/simplesearch/search/storage/IndexPartialStorage.java
@@ -1,14 +1,18 @@
 package team.snof.simplesearch.search.storage;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Component;
+import team.snof.simplesearch.search.model.dao.index.Index;
 import team.snof.simplesearch.search.model.dao.index.IndexPartial;
+import team.snof.simplesearch.search.model.dao.index.TempData;
 
+import java.time.temporal.TemporalAccessor;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -46,11 +50,18 @@ public class IndexPartialStorage {
         return wordDocNumMap;
     }
 
-    // 更新操作 若已经存在则是对list扩充 不存在就插入
+    // 若不存在word记录 则新增存储  若存在则读word对应的list进行扩充更新
     public void saveIndexPartial(IndexPartial indexPartial) {
         Query query = new Query().addCriteria(Criteria.where("indexKey").is(indexPartial.getIndexKey()));
-        Update update = new Update();
-        update.set("tempDataList", indexPartial.getTempDataList());
-        mongoTemplate.upsert(query, update, IndexPartial.class, "word_temp");
+        IndexPartial wordIndexPartial = mongoTemplate.findOne(query, IndexPartial.class, "word_temp");
+        if (wordIndexPartial == null) {
+            mongoTemplate.save(indexPartial, "word_temp");
+        } else {
+            List<TempData> tempDataList = indexPartial.getTempDataList();
+            wordIndexPartial.getTempDataList().addAll(tempDataList);
+            Update update = new Update();
+            update.set("tempDataList", wordIndexPartial.getTempDataList());
+            mongoTemplate.updateFirst(query, update, IndexPartial.class, "word_temp");
+        }
     }
 }
